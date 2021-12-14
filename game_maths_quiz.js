77386a3bab1c3f9ac5430432df08847f5849e9cf
nikhilkumar031982 <91476583+nikhilkumar031982@users.noreply.github.com>
2021-12-14 16:45:40 +0530

    Add files via upload

---
+++ game_maths_quiz.js
@@ -0,0 +1,6 @@
+function adduser(){
+player_1=document.getElementById("textinput1").value;
+    player_1=document.getElementById("textinput2").value;
+    localStorage.setItem(player_1);
+    localStorage.setItem(player_2);
+}
